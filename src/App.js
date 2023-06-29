345460949503e941d37222386f1086f4adb9bd40
deanw <deanwademosley@gmail.com>
2023-06-28 20:51:35 -0500

    "attempting to fix FETCH_ALL error"

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useEffect} from "react";
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from "react-redux";
 
+import { getPosts } from './actions/posts.js'
 import Posts from "./components/Posts/Posts.js"
 import Form from "./components/Form/Form.js"
 
@@ -9,6 +11,12 @@ import useStyles from "./styles.js"
 
 const App = () => {
     const classes = useStyles();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPosts());
+    },[dispatch]);
+
     return (
         <Container position> 
             <AppBar className={classes.appBar} position="static" color="inherit">
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { useSelector } from "react-redux";
+
 import Post from "./Post/post"
 
 import useStyles from "./styles.js"
-const Posts = () => {
 
+
+const Posts = () => {
+    const posts = useSelector((state) => state.posts);
     const classes = useStyles();
+
+    console.log(posts);
+
     return (
         <>
             <h1>POSTS</h1>
